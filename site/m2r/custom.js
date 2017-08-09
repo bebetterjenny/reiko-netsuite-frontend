e3f86f3368d0e7698390df04e0b1641c196cb138
Jenny Han <bebetterjenny@gmail.com>
2017-08-09 14:06:55 -0400

    fix minor issue for workflow

--- site/m2r/custom.js
+++ site/m2r/custom.js
@@ -60,9 +60,14 @@ $(".selfanchor").click(function(){
 /************************************************************
 ***********************   workflow  *************************
 **********************************************************/
+const reset = ""+$("#oversea-remove p").html();
 $('.workflow-tab a[href="#mix"]').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
+    $("#workflow .m2r-empty").css({"visibility": "visible"});
+    $("#workflow .oversea-empty").css({"visibility": "visible"});
+    $("#oversea-remove").addClass("reiko-do col-xs-2 col-sm-2 col-md-2 col-lg-2").removeClass("arrow arrow-left col-xs-1 col-sm-1 col-md-1 col-lg-1").next().removeClass("col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
+    if($("#oversea-remove p").length == 0) $("#oversea-remove").append("<p class='bg-primary'>"+reset+"</p>");
     $("#workflow .row .manu-do").css({
         "background-color": "rgba(88,122,165,0.8)"
     });
@@ -76,6 +81,7 @@ $('.workflow-tab a[href="#mix"]').click(function (e) {
 $('.workflow-tab a[href="#oversea"]').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
+    $("#workflow .m2r-empty").css({"visibility": "visible"});
     $("#workflow .row .manu-do").css({
         "background-color": "rgba(243,124,61,0.8)"
     });
@@ -85,10 +91,17 @@ $('.workflow-tab a[href="#oversea"]').click(function (e) {
     $("#workflow .row .wrong-do").css({
         "background-color": "rgba(243,124,61,0.8)"
     });
+    $("#workflow .oversea-empty").css({"visibility": "hidden"});
+    $("#oversea-remove p").remove("p");
+    $("#oversea-remove").removeClass("reiko-do col-xs-2 col-sm-2 col-md-2 col-lg-2").addClass("arrow arrow-left col-xs-1 col-sm-1 col-md-1 col-lg-1").css({"background-color":"#fff"}).next().addClass("col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
 });
 $('.workflow-tab a[href="#m2r"]').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
+    $("#workflow .m2r-empty").css({"visibility": "hidden"});
+    $("#workflow .oversea-empty").css({"visibility": "visible"});
+    $("#oversea-remove").addClass("reiko-do col-xs-2 col-sm-2 col-md-2 col-lg-2").removeClass("arrow arrow-left col-xs-1 col-sm-1 col-md-1 col-lg-1").next().removeClass("col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
+    if($("#oversea-remove p").length < 1) $("#oversea-remove").append("<p class='bg-primary'>"+reset+"</p>");
     $("#workflow .row .manu-do").css({
         "background-color": "rgba(88,122,165,0.8)"
     });
